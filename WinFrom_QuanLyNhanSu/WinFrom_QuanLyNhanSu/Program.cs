18f437a1cd86e150396e1e4c35078521bf307638
quyetthang96 <quyetthang326@gmail.com>
2017-05-22 01:01:49 +0700

    Hoan thanh them nv

--- WinFrom_QuanLyNhanSu/WinFrom_QuanLyNhanSu/Program.cs
+++ WinFrom_QuanLyNhanSu/WinFrom_QuanLyNhanSu/Program.cs
@@ -16,7 +16,7 @@ namespace WinFrom_QuanLyNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormQLNhanVien());
         }
     }
 }
